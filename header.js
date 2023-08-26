f874d81c2ce88657a9c172c2fdc5e45c597ebb57
ImobilarTCC <137441665+ImobilarTCC@users.noreply.github.com>
2023-08-26 17:34:13 -0300

    Add files via upload

---
+++ header.js
@@ -0,0 +1,29 @@
+let headerMain = document.querySelector(".header-main");
+let headerMobileBtn = document.querySelector(".mobile-menu-btn");
+let headerMobileOverlay = document.querySelector(".mobile-menu-overlay");
+const width = window.innerWidth;
+
+let menuOpen = false;
+
+headerMobileBtn.onclick = function () {
+    if (!menuOpen) {
+        headerMain.style.display = "flex";
+        headerMobileOverlay.style.display = "flex";
+        menuOpen = true;
+    } else {
+        headerMain.style.display = "none";
+        headerMobileOverlay.style.display = "none";
+        menuOpen = false;
+    }
+}
+
+var mediaQuery = window.matchMedia('(min-width: 993px)');
+mediaQuery.addEventListener('change', function (e) {
+    if (e.matches) {
+        headerMain.style.display = "flex";
+        headerMobileOverlay.style.display = "none";
+    } else {
+        headerMain.style.display = "none";
+        headerMobileOverlay.style.display = "none";
+    }
+});
