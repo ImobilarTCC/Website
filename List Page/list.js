dbe2fd43b228f3872a78d8e63b8ef965ecf292ca
ImobilarTCC <137441665+ImobilarTCC@users.noreply.github.com>
2023-08-29 22:28:20 -0300

    Add files via upload

--- List Page/list.js
+++ List Page/list.js
@@ -1,7 +1,8 @@
-let getFormContent = document.querySelector(".filtro_form");
 let menuOpenFiltro = false;
 
 let toggleFiltroMenu = function (){
+
+    let getFormContent = document.querySelector(".filtro_form");
 
     let width = window. innerWidth
 
@@ -24,6 +25,9 @@ let toggleFiltroMenu = function (){
 
 let mediaQueryFiltro = window.matchMedia('(max-width: 1200px)');
 mediaQueryFiltro.addEventListener('change', function (e) {
+
+    let getFormContent = document.querySelector(".filtro_form");
+
     if (e.matches) {
         getFormContent.style.maxHeight = "0px";
         getFormContent.style.opacity = "0";
@@ -38,3 +42,45 @@ mediaQueryFiltro.addEventListener('change', function (e) {
         menuOpenFiltro = false;
     }
 });
+
+let changeImg = function(){
+    let getImgs = document.querySelectorAll(".filtro_extras img");
+    let getCheckbox = document.querySelectorAll(".filtro_extras input");
+    if (getCheckbox[0].checked) {
+        getImgs[0].src = "img/wifiSim.png";
+    } else {
+        getImgs[0].src = "img/wifi.png";
+    }
+
+    if (getCheckbox[1].checked) {
+        getImgs[1].src = "img/refeicaoSim.png";
+    } else {
+        getImgs[1].src = "img/refeicao.png";
+    }
+
+    if (getCheckbox[2].checked) {
+        getImgs[2].src = "img/lazerSim.png";
+    } else {
+        getImgs[2].src = "img/lazer.png";
+    }
+
+    if (getCheckbox[3].checked) {
+        getImgs[3].src = "img/estacionamentoSim.png";
+    } else {
+        getImgs[3].src = "img/estacionamento.png";
+    }
+
+    if (getCheckbox[4].checked) {
+        getImgs[4].src = "img/animalSim.png";
+    } else {
+        getImgs[4].src = "img/animal.png";
+    }
+}
+
+let changeFavorite = function (clickedImg) {
+    if (clickedImg.getAttribute("src") == "img/favorito.png"){
+        clickedImg.src = "img/favoritoRed.png";
+    } else {
+        clickedImg.src = "img/favorito.png";
+    }
+}
--- header.js
+++ header.js
@@ -1,17 +1,21 @@
-let getHeaderMain = document.querySelector(".header-main");
-let getHeaderMobileOverlay = document.querySelector(".mobile-menu-overlay");
-let getHeaderLogo = document.querySelector(".header-logo");
-let getHeaderNav = document.querySelector(".header-navigation");
-let getHeaderIcons = document.querySelector(".header-icons");
-
 let menuOpenHeader = false;
 
 let toggleMenu = function () {
 
+    let getHeaderMain = document.querySelector(".header-main");
+    let getHeaderMenuLogo = document.querySelector(".mobile-menu-logo");
+    let getHeaderMenuButton = document.querySelector(".mobile-menu-menuButton");
+    let getHeaderExitButton = document.querySelector(".mobile-menu-exitButton");
+    let getHeaderMobileOverlay = document.querySelector(".mobile-menu-overlay");
+    let getHeaderLogo = document.querySelector(".header-logo");
+    let getHeaderNav = document.querySelector(".header-navigation");
+    let getHeaderIcons = document.querySelector(".header-icons");
+
     if (!menuOpenHeader) {
-        getHeaderMain.style.width = "85%";
-        getHeaderMain.style.borderRight = "3px solid #154360";
-        getHeaderMain.style.transition = "all 0.4s linear";
+        getHeaderMenuLogo.style.visibility = "hidden";
+        getHeaderMain.style.width = "75%";
+        getHeaderMain.style.borderRight = "2px solid #154360";
+        getHeaderMain.style.transition = "all 0.4s ease-in-out";
         getHeaderLogo.style.opacity = "1";
         getHeaderNav.style.opacity = "1";
         getHeaderIcons.style.opacity = "1";
@@ -20,8 +24,11 @@ let toggleMenu = function () {
         getHeaderIcons.style.visibility = "visible";
         getHeaderMobileOverlay.style.display = "flex";
         document.body.style.overflow = "hidden";
+        getHeaderMenuButton.style.display = "none";
+        getHeaderExitButton.style.display = "flex";
         menuOpenHeader = true;
     } else {
+        getHeaderMenuLogo.style.visibility = "visible";
         getHeaderMain.style.borderRight = "0px solid transparent";
         getHeaderLogo.style.opacity = "0";
         getHeaderNav.style.opacity = "0";
@@ -32,13 +39,28 @@ let toggleMenu = function () {
         getHeaderMobileOverlay.style.display = "none";
         document.body.style.overflow = "auto";
         getHeaderMain.style.width = "0%";
+        getHeaderMenuButton.style.display = "flex";
+        getHeaderExitButton.style.display = "none";
         menuOpenHeader = false;
     }
 }
 
 let mediaQueryHeader = window.matchMedia('(min-width: 993px)');
 mediaQueryHeader.addEventListener('change', function (e) {
+
+    let getHeaderMain = document.querySelector(".header-main");
+    let getHeaderMenuLogo = document.querySelector(".mobile-menu-logo");
+    let getHeaderMenuButton = document.querySelector(".mobile-menu-menuButton");
+    let getHeaderExitButton = document.querySelector(".mobile-menu-exitButton");
+    let getHeaderMobileOverlay = document.querySelector(".mobile-menu-overlay");
+    let getHeaderLogo = document.querySelector(".header-logo");
+    let getHeaderNav = document.querySelector(".header-navigation");
+    let getHeaderIcons = document.querySelector(".header-icons");
+
     if (e.matches) {
+        getHeaderMenuLogo.style.visibility = "visible";
+        getHeaderMenuButton.style.display = "flex";
+        getHeaderExitButton.style.display = "none";
         getHeaderMain.style.width = "100%";
         getHeaderMain.style.borderRight = "0px solid transparent";
         getHeaderMain.style.transition = "unset";
@@ -49,7 +71,8 @@ mediaQueryHeader.addEventListener('change', function (e) {
         getHeaderNav.style.visibility = "visible";
         getHeaderIcons.style.visibility = "visible";
         document.body.style.overflow = "auto";
-    } else {
+        menuOpenHeader = false;
+    } else { 
         getHeaderMain.style.width = "0%";
         getHeaderMain.style.borderRight = "0px solid transparent";
         getHeaderMain.style.transition = "unset";
@@ -61,6 +84,6 @@ mediaQueryHeader.addEventListener('change', function (e) {
         getHeaderIcons.style.visibility = "hidden";
         getHeaderMobileOverlay.style.display = "none";
         document.body.style.overflow = "auto";
-        menuOpen = false;
+        menuOpenHeader = false;
     }
 });
