f04db82cc21a3be39546a7aa06aeeb44a9a0eeef
ImobilarTCC <137441665+ImobilarTCC@users.noreply.github.com>
2023-09-23 16:57:12 -0300

    Add files via upload

--- Create Page/create.js
+++ Create Page/create.js
@@ -31,3 +31,49 @@ let changeImg = function(){
         getImgs[4].src = "img/Animals.png";
     }
 }
+
+let formatCep = function(cep){
+    cep = cep.replace(/\D/g,''); // Replace all chars to numbers
+    let size = cep.length; // get input lenght
+    if (size>5) { // if is higher than 5 chars
+        cep = cep.slice(0,5)+"-"+cep.slice(5,8) // the input get a '-' after the first 5 chars
+    }
+    return cep; // return the input
+}
+
+const currencyInputs = document.querySelectorAll('#input_valor');
+
+currencyInputs.forEach(input => {
+    const max = parseFloat(input.getAttribute('data-max'));
+
+    input.addEventListener('input', formatCurrency);
+
+    function formatCurrency() {
+        let rawValue = input.value.replace(/[^0-9]/g, '');
+
+        // Handle the case when all numbers are deleted and NaN is generated
+        if (rawValue === '') {
+            rawValue = 0;
+        } else {
+            // Limit the raw value to the specified maximum
+            rawValue = Math.min(max, parseFloat(rawValue));
+        }
+
+        const formattedValue = formatCurrencyValue(rawValue);
+
+        input.value = formattedValue;
+    }
+
+    function formatCurrencyValue(rawValue) {
+        const numericValue = Number(rawValue) / 100;
+
+        const formattedValue = numericValue.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+        });
+
+        return formattedValue;
+    }
+});
