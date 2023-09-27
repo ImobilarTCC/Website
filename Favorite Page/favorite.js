f70001fe36f24c9ee14e72659ed971e9af51363e
ImobilarTCC <137441665+ImobilarTCC@users.noreply.github.com>
2023-09-27 11:07:16 -0300

    Delete Favorite Page directory

--- Favorite Page/favorite.js
+++
@@ -1,7 +0,0 @@
-let changeFavorite = function (clickedImg) {
-    if (clickedImg.getAttribute("src") == "img/favorito.png"){
-        clickedImg.src = "img/favoritoRed.png";
-    } else {
-        clickedImg.src = "img/favorito.png";
-    }
-}
